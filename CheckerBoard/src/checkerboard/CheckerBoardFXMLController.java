83346416ee7adbc402aad92e74bbdde33403745d
Hunter Ginther <hagd5d@mail.missouri.edu>
2018-01-26 18:43:53 -0600

    Initial
    Initial commit

---
+++ CheckerBoard/src/checkerboard/CheckerBoardFXMLController.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package checkerboard;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author hunterginther
+ */
+public class CheckerBoardFXMLController implements Initializable, Startable {
+    
+    private Stage stage;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    } 
+    
+    @Override
+    public void start(Stage stage) {
+        
+    }
+    
+}
