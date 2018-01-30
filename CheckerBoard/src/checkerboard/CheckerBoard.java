8546a8ee68a8dbb201b52a026cd387d4281ba5c7
Hunter Ginther <hagd5d@mail.missouri.edu>
2018-01-29 21:33:25 -0600

    Working Checkerboard

--- CheckerBoard/src/checkerboard/CheckerBoard.java
+++ CheckerBoard/src/checkerboard/CheckerBoard.java
@@ -6,6 +6,9 @@
 package checkerboard;
 
 import javafx.application.Application;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -17,6 +20,92 @@ import javafx.stage.Stage;
  */
 public class CheckerBoard extends Application {
     
+    private int numRows;
+    private int numCols;
+    private double boardWidth;
+    private double boardHeight;
+    private Color lightColor;
+    private Color darkColor;
+    
+    private AnchorPane board = null;
+    private double rectWidth;
+    private double rectHeight;
+    
+    public CheckerBoard() {
+        
+    }
+    
+    public CheckerBoard(int numRows, int numCols, double boardWidth, double boardHeight) {
+        this.numRows = numRows;
+        this.numCols = numCols;
+        this.boardWidth = boardWidth;
+        this.boardHeight = boardHeight;
+    }
+    
+    public CheckerBoard(int numRows, int numCols, double boardWidth, double boardHeight, Color lightColor, Color darkColor) {
+        this(numRows, numCols, boardWidth, boardHeight);
+        this.lightColor = lightColor;
+        this.darkColor = darkColor;
+    }
+    
+    public AnchorPane build() {
+        rectWidth = (boardWidth / (double) numCols);
+        rectHeight = (boardHeight / (double) numRows);
+        
+        board = new AnchorPane();
+
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numCols; col++) {
+                Rectangle rect = new Rectangle(rectWidth * col, rectHeight * row, rectWidth, rectHeight);
+                
+                if ((row % 2) == (col % 2)) {
+                    rect.setFill(lightColor);
+                } else {
+                    rect.setFill(darkColor);
+                }
+
+                board.getChildren().add(rect);
+            }
+        }
+        return board;
+    }
+    
+    public AnchorPane getBoard() {
+        return this.board;
+    }
+    
+    public int getNumRows() {
+        return this.numRows;
+    }
+    
+    public int getNumCols() {
+        return this.numCols;
+    }
+    
+    public double getWidth() {
+        return this.boardWidth;
+    }
+    
+    public double getHeight() {
+        return this.boardHeight;
+    }
+    
+    public Color getLigthColor() {
+        return this.lightColor;
+    }
+    
+    public Color getDarkColor() {
+        return this.darkColor;
+    }
+    
+    public double getRectangleWidth() {
+        return this.rectWidth;
+    }
+    
+    public double getRectangleHeight() {
+        return this.rectHeight;
+    }   
+
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("CheckerBoardFXML.fxml"));
@@ -29,12 +118,12 @@ public class CheckerBoard extends Application {
         stage.show();
         controller.start(stage);
     }
-
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
--- CheckerBoard/src/checkerboard/CheckerBoardFXMLController.java
+++ CheckerBoard/src/checkerboard/CheckerBoardFXMLController.java
@@ -9,7 +9,14 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.Initializable;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
@@ -18,7 +25,26 @@ import javafx.stage.Stage;
  */
 public class CheckerBoardFXMLController implements Initializable, Startable {
     
+    private int numRows = 8;
+    private int numCols = 8;
+    private Color lightColor = Color.RED;
+    private Color darkColor = Color.BLACK;
+    
     private Stage stage;
+    
+    @FXML
+    private AnchorPane anchorPane;
+    
+    @FXML
+    private VBox vbox;
+    
+    @FXML
+    private MenuBar menuBar;
+
+    @FXML private MenuItem grid16;
+    @FXML private MenuItem grid10;
+    @FXML private MenuItem grid8;
+    @FXML private MenuItem grid3;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -27,7 +53,68 @@ public class CheckerBoardFXMLController implements Initializable, Startable {
     
     @Override
     public void start(Stage stage) {
+        this.stage = stage;
         
+        ChangeListener<Number> lambdaChangeListener = (ObservableValue<? extends Number> observable, Number oldValue, final Number newValue) -> {
+            refreshBoard();
+        };
+        this.stage.widthProperty().addListener(lambdaChangeListener);
+        this.stage.heightProperty().addListener(lambdaChangeListener);
+        
+        refreshBoard();
     }
     
+    private void refreshBoard() {
+        double boardWidth = this.vbox.getWidth();
+        double boardHeight = this.vbox.getHeight() - menuBar.getHeight();
+        
+        CheckerBoard cb = new CheckerBoard(numRows, numCols, boardWidth, boardHeight, lightColor, darkColor);
+        AnchorPane ap = cb.build();
+         
+        anchorPane.getChildren().clear();
+        
+        anchorPane.getChildren().addAll(ap);
+    }
+    
+    @FXML 
+    private void handleGridSize(ActionEvent event) {
+        MenuItem sizeSelection = (MenuItem)(event.getSource()); 
+        
+        switch (sizeSelection.getId()) {
+            case "grid16":
+                numRows = 16;
+                numCols = 16;
+                break;
+            case "grid10":
+                numRows = 10;
+                numCols = 10;
+                break;
+            case "grid3":
+                numRows = 3;
+                numCols = 3;
+                break;
+            default:
+                numRows = 8;
+                numCols = 8;
+                break;
+        }
+        
+        refreshBoard();
+    }
+    
+    @FXML
+    private void handleColorDefault(ActionEvent event) {
+        lightColor = Color.RED;
+        darkColor = Color.BLACK;
+        
+        refreshBoard();
+    }
+    
+    @FXML
+    private void handleColorBlue(ActionEvent event) {
+        lightColor = Color.SKYBLUE;
+        darkColor = Color.DARKBLUE;
+        
+        refreshBoard();
+    }
 }
